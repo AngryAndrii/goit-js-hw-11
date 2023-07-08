d745e79cb2cc506b6bc147b6708026314c375a81
Andrii Chychkan <mywork1728@gmail.com>
2023-07-08 23:09:14 +0300

    started async

--- src/script.js
+++ src/script.js
@@ -1,2 +1,10 @@
 //38146382-d0cd2611e4be2665aae5df53b
-console.log('hello bastards');
+fetch(
+  `https://pixabay.com/api/?key=38146382-d0cd2611e4be2665aae5df53b&q=yellow+flowers&image_type=photo?_limit=2`
+)
+  .then(resp => {
+    return resp.json();
+  })
+  .then(data => {
+    console.log(data);
+  });
